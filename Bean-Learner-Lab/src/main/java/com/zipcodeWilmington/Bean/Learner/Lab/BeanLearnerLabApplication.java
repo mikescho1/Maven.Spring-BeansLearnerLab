1de7f6dd9b1320d3391374f35c62805ae58ae3e2
mike <mike.schoenbeck@gmail.com>
2019-12-09 12:17:32 -0500

    created project

---
+++ Bean-Learner-Lab/src/main/java/com/zipcodeWilmington/Bean/Learner/Lab/BeanLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package com.zipcodeWilmington.Bean.Learner.Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanLearnerLabApplication.class, args);
+	}
+
+}
---
+++ Bean-Learner-Lab/src/test/java/com/zipcodeWilmington/Bean/Learner/Lab/BeanLearnerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcodeWilmington.Bean.Learner.Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLearnerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
